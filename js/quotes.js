482b68df5168bac2a21af3f4805fd71f676d498b
leejaelll <jayaom91076@gmail.com>
2022-11-24 13:06:54 +0900

    feat: Correct quotes.js
    - qutes 배열 요소 변경

--- js/quotes.js
+++ js/quotes.js
@@ -1,42 +1,15 @@
 const quotes = [
-    {
-        quotes: "You know that children are growing up when they start asking questions that have answers.",
-        author: "John J. Plomp",
-    },
-    {
-        quotes: "Learning without thought is labor lost; thought without learning is perilous.",
-        author:"Confucius",
-    },
-    {
-        quotes: "Seeing much, suffering much, and studying much, are the three pillars of learning.",
-        author: "Benjamin Disraeli",
-    },
-    {
-        quotes: "That is what learning is. You suddenly understand something you've understood all your life, but in a new way.",
-        author: "Doris Lessing",
-    },
-    {
-        quotes: "Life is not fair; get used to it.",
-        author: "Bill Gates",
-    },
-    {
-        quotes: "Life is pleasant. Death is peaceful. It's the transition that's troublesome.",
-        author: "Isaac Asimov",
-    },
-    {   
-        quotes: "They must often change who would be constant in happiness or wisdom.",
-        author: "Confucius",
-    },
-    {
-        quotes: "The dead might as well try to speak to the living as the old to the young.",
-        author: "Willa Cather",
-    },
-]
+  '지금 공부 안 하면 더울 때 더운 데서 일하고 추울 때 추운 데서 일한다.',
+  '늦었다고 생각할 때가 늦었다',
+  '“내 너 그럴 줄 알았다” 알았으면 제발 미리 말 해줘라',
+  '성공은 1% 재능과 99%의 빽',
+  '내일도 할 수 있는 일을 굳이 오늘 할 필요 없다 ',
+  '가는 말이 고우면 얕본다',
+  '인생을 짧고... 열심히 해라',
+  '꿈은 없고요 그냥 놀고싶습니다. ',
+];
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const quote = document.querySelector('#quote span:first-child');
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
-
-quote.innerText = todaysQuote.quotes;
-author.innerText = todaysQuote.author;
+quote.textContent = todaysQuote;
