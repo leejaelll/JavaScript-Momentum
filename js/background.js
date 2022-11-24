c8cbcd9006bc097c2c962d82de678d7b566ee4b2
leejaelll <jayaom91076@gmail.com>
2022-11-24 13:07:40 +0900

    feat: Correct background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,23 @@
-const images = ["0.jpeg", "1.jpeg","2.jpeg"];
+const images = [
+  '1.jpeg',
+  '2.jpeg',
+  '3.jpeg',
+  '4.jpeg',
+  '5.jpeg',
+  '6.jpeg',
+  '7.jpeg',
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+console.log(chosenImage);
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement('img');
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+window.addEventListener('load', () => {
+  document.body.style.backgroundImage = `url('../img/${chosenImage}')`;
+  document.body.style.backgroundSize = 'cover';
+});
